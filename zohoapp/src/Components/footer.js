4480353699aaa94ca405e9ab1589bd178e9f6498
shivamd21 <shivam.d9521@gmail.com>
2020-08-27 02:44:31 +0530

    zoho app

---
+++ zohoapp/src/Components/footer.js
@@ -0,0 +1,156 @@
+import React from 'react'
+import './CSS/footer.css'
+
+function Footer() {
+    return (
+        <div>
+            <footer class="page-footer bg-dark myfooter">
+
+                <div class="container text-center  mx-auto mr-auto text-md-left mt-8">
+                    <div class="row">
+
+
+                        <div class="col-md-2 mx-auto mb-4">
+                            <h5 class=" font-weight-bold">Apps</h5  >
+                            <ul class="list-unstyled">
+
+                                <li class="my-2">  <a href="#" className=" brand  text-white colbot" >Mobile Apps</a></li>
+                                <li class="my-2"> <a href="#" className=" brand text-white colbot">G Suite Integration</a></li>
+                                <li class="my-2"> <a href="#" className=" brand text-white colbot">Microsoft office 365 Integration</a></li>
+                                <li class="my-2"> <a href="#" className=" brand text-white colbot">Apps for Apple Watch</a></li>
+
+                            </ul>
+                        </div>
+
+                        <div class="col-md-2 mx-auto mb-4">
+                            <h5 class=" font-weight-bold">Partners</h5>
+                            <hr class="bg-success mb-4 mt-0 d-inline-block mx-auto" />
+
+                            <ul class="list-unstyled">
+
+                                <li class="my-2"> <a href="#" className=" brand  text-white colbot ">Hosting Providers</a></li>
+                                <li class="my-2">  <a href="#" className=" brand text-white colbot">Partners</a></li>
+                                <li class="my-2"><a href="#" className=" brand text-white colbot ">Consulting  Partners</a></li>
+                                <li class="my-2"> <a href="#" className=" brand text-white colbot">Platform  Partners</a></li>
+                                <li class="my-2"><a href="#" className=" brand text-white colbot">Technology  Partners</a></li>
+
+                            </ul>
+                        </div>
+
+                        <div class="col-md-2 mx-auto mb-4">
+                            <h5 class=" font-weight-bold">Resources</h5>
+                            <hr class="bg-success mb-4 mt-0 d-inline-block mx-auto" />
+                            <ul class="list-unstyled">
+                                <li class="my-2">   <a href="#" className=" brand  text-white colbot ">Zia</a></li>
+                                <li class="my-2">  <a href="#" className=" brand text-white colbot">Support</a></li>
+                                <li class="my-2">  <a href="#" className=" brand text-white colbot ">Community</a></li>
+                                <li class="my-2"> <a href="#" className=" brand text-white ">Forum</a></li>
+                                <li class="my-2">  <a href="#" className=" brand text-white colbot ">Blogs</a></li>
+                                <li class="my-2"> <a href="#" className=" brand text-white colbot ">Academy</a></li>
+                                <li class="my-2">  <a href="#" className=" brand text-white colbot ">Newsletter</a></li>
+
+                            </ul>
+                        </div>
+
+                        <div class="col-md-2 mx-auto mb-4">
+                            <h5 class="font-weight-bold">About</h5>
+                            <hr class="bg-success mb-4 mt-0 d-inline-block mx-auto" />
+                            <ul class="list-unstyled">
+
+
+                                <li class="my-2">  <a href="#" className=" brand  text-white colbot ">About Zoho</a></li>
+                                <li class="my-2">  <a href="#" className=" brand text-white colbot ">Perspectives</a></li>
+                                <li class="my-2"><a href="#" className=" brand text-white colbot ">Events</a></li>
+                                <li class="my-2"><a href="#" className=" brand text-white colbot">Press</a></li>
+                                <li class="my-2"><a href="#" className=" brand text-white colbot">Zoho in the news</a></li>
+                                <li class="my-2"> <a href="#" className=" brand text-white colbot">Zoho opiions</a></li>
+                                <li class="my-2"> <a href="#" className=" brand text-white colbot">Zoho School</a></li>
+                                <li class="my-2">  <a href="#" className=" brand text-white colbot">Contact us</a></li>
+                            </ul>
+                        </div>
+
+                        <div class="col-md-2 mx-auto mb-4">
+                            <h5 class=" font-weight-bold">Contact-India</h5>
+                            <hr class="bg-success mb-4 mt-0 d-inline-block mx-auto" />
+                            <b>GST Helpline</b> <br />
+                            <ul class="list-unstyled">
+                                <li class="my-2">  <a href="tel:1800 572 6671" className=" brand text-white colbot ">1800 572 6671</a></li>
+                                <li class="my-2">  <a href="mailto:gstindia@zohofinance.com" className=" brand text-white colbot ">gstindia@zohofinance.com</a></li>
+                            </ul>
+                            <b>Sales</b> <br />
+                            <ul class="list-unstyled">
+                                <li class="my-2"><a href="tel:1800 103 1123" className=" brand text-white colbot ">1800 103 1123</a></li>
+                                <li class="my-2"><a href="tel:+91 (44) 67447000" className=" brand text-white colbot">+91 (44) 67447000</a></li>
+                                <li class="my-2"><a href="mailto:sales@zohocorp.com" className=" brand text-white colbot">sales@zohocorp.com</a></li>
+                            </ul>
+                        </div>
+
+
+                    </div></div>
+
+
+
+                <div class="container">
+                    <div class="row py-4 d-flex align-items-center">
+
+                        <div class="col-md-12 text-center">
+                            <form class="form-inline my-2 my-lg-0">
+                                <input className=" searchinput" type="search" placeholder="Search for product overviews , FAQs,and more" aria-label="Search" />&nbsp;&nbsp;&nbsp;&nbsp;
+<a className="brand text-white colbot " href="#" type="submit"><i className="fa fa-search"></i></a>
+                            </form>     </div>
+
+                    </div>
+                </div>
+
+
+                <div class="container">
+                    <div class="row py-4 d-flex align-items-center">
+
+                        <div class="col-md-12 text-center">
+                            <a className="text-white " href="#">Zono Home</a> &nbsp;&nbsp;|&nbsp;&nbsp;
+  <a className="text-white " href="#">ContactUS</a> &nbsp;&nbsp; |&nbsp;&nbsp;
+  <a className="text-white " href="#">Security</a>&nbsp;&nbsp; |&nbsp;&nbsp;
+  <a className="text-white " href="#">IPR Complaints</a>&nbsp;&nbsp; |&nbsp;&nbsp;
+  <a className="text-white " href="#">Anti-Spam Policy</a>&nbsp;&nbsp; |&nbsp;&nbsp;
+  <a className="text-white " href="#">Terms of Service</a> &nbsp;&nbsp;|&nbsp;&nbsp;
+  <a className="text-white " href="#">Privace Policy</a>&nbsp;&nbsp; |&nbsp;&nbsp;
+  <a className="text-white " href="#">Cookie Policy</a> &nbsp;&nbsp;|&nbsp;&nbsp;
+  <a className="text-white " href="#">GDPR Compliance</a>&nbsp;&nbsp; |&nbsp;&nbsp;
+  <a className="text-white " href="#">Abuse Policy</a>    </div>
+
+                    </div>
+                </div>
+
+                <div class="container">
+                    <div class="row py-4 d-flex align-items-center">
+
+                        <div class="col-md-12 text-center">
+                            © 2020, Zoho Corporation Pvt. Ltd. All Rights Reserved.     </div>
+
+                    </div>
+                </div>
+
+                <div class="container">
+                    <div class="row py-4 d-flex align-items-center">
+
+                        <div class="col-md-12 text-center">
+                            <a href="#"><i class="fab fa-facebook-f white-text mr-4 socialicon"> </i></a>
+                            <a href="#"><i class="fab fa-twitter white-text mr-4 socialicon"> </i></a>
+                            <a href="#"><i class="fab fa-linkedin-in white-text mr-4 socialicon"> </i></a>
+                            <a href="#"><i class="fab fa-youtube white-text socialicon"> </i></a>
+                        </div>
+
+                    </div>
+                </div>
+
+
+
+
+
+            </footer>
+        </div>
+    )
+}
+
+export default Footer
+
